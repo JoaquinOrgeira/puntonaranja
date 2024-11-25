853eb4f3daa1fc0464686d61eb499a9c8cc0d059
JoaquinOrgeira <orgeirajoaquin@gmail.com>
2024-11-25 15:42:15 -0800

    Create script.php

---
+++ script.php
@@ -0,0 +1,28 @@
+<?php
+// Definimos 20 variables
+$var1 = 1;
+$var2 = 2;
+$var3 = 3;
+$var4 = 4;
+$var5 = 5;
+$var6 = 6;
+$var7 = 7;
+$var8 = 8;
+$var9 = 9;
+$var10 = 10;
+$var11 = 11;
+$var12 = 12;
+$var13 = 13;
+$var14 = 14;
+$var15 = 15;
+$var16 = 16;
+$var17 = 17;
+$var18 = 18;
+$var19 = 19;
+$var20 = 20;
+
+// Seleccionamos una variable y la multiplicamos por 2
+$valor = $var5; // Cambia esto a la variable que quieras usar
+$resultado = $valor * 2;
+
+?>
